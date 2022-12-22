eab4085a0ce79ca1bd9e5a8698dc6521362c9d92
ChristianOConnor <warpanomaly@gmail.com>
2022-12-21 20:19:54 -0500

    change button to a tag and define character type

--- src/pages/characters.tsx
+++ src/pages/characters.tsx
@@ -3,6 +3,12 @@ import React, { useEffect, useState, useCallback } from "react";
 import Layout from "../components/layout";
 import * as styles from '../../styles/Home.module.css';
 
+interface Character {
+    "id": string,
+    "name": string,
+    "species": string,
+    "status": string
+}
 
 const AquireData = async (params: any) => {
     let inputParam = 1;
@@ -40,21 +46,8 @@ const AquireData = async (params: any) => {
 }
 
 const Characters: React.FC<PageProps> = () => {
-    const starterJsonPreParse = `[
-        {
-            "id": "1",
-            "name": "Rick Sanchez",
-            "species": "Human",
-            "status": "Alive"
-        },
-        {
-            "id": "2",
-            "name": "Morty Smith",
-            "species": "Human",
-            "status": "Alive"
-        }
-    ]`;
-    const starterJson = JSON.parse(starterJsonPreParse);
+    const starterJsonPreParse: Character[] = [];
+    const starterJson = starterJsonPreParse;
     const [pageUpVal, setPageUpVal] = useState('/characters');
     const [pageDownVal, setPageDownVal] = useState('/characters');
     const [fetchedParamsNum, setFetchedParamsNum] = useState(1);
@@ -103,18 +96,14 @@ const Characters: React.FC<PageProps> = () => {
                 </div>
                 <br/>
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
-                    <button onClick={(ev) => {
-                        window.location = pageDownVal as any;
-                    }} className="inline-flex items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    <a href={pageDownVal} className="inline-flex items-center px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         <svg aria-hidden="true" className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"></path></svg>
                         Previous
-                    </button>
-                    <button onClick={(ev) => {
-                        window.location = pageUpVal as any;
-                    }} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                    </a>
+                    <a href={pageUpVal} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         Next
                         <svg aria-hidden="true" className="w-5 h-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                    </button>
+                    </a>
                 </div>
             </div>
             <br />
